f0c56471e7a5f70a8fa3f9019739671ec0ffbba2
bigboy <bigboy.061293@gmail.com>
2019-03-18 10:12:58 +0700

    Add new files

---
+++ dumboo/dumb1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+   //#ifdef __cplusplus
+   //cout << "C++ compiler in use and version is " << __cplusplus << endl;
+   //#endif
+	cout << "^^" << endl;
+  // cout <<"Version is " << __STDC_VERSION__ << endl;
+  // cout << "Hi" << __FILE__ << __LINE__ << endl;
+}
